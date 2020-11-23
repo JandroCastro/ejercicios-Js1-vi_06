6419f1b07b46792bf92b2357a237c4cce0103e8b
Alejandro Castro <jandro.castro.alarcon@hackaboss.com>
2020-11-23 18:12:04 +0100

    ejercicio tres

--- ejercicio4.js
+++ ejercicio4.js
@@ -1,4 +1,5 @@
-// Try to show all the characters that are human by consuming the Rick and Morty API
+// Try to show all the characters that are human 
+// by consuming the Rick and Morty API
 
 // Rick and Morty API:
 // https://rickandmortyapi.com/documentation/#rest
